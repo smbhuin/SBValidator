c097135135b08b0ca648d5db9270c7be91b82ed2
Soumen Bhuin <smbhuin@gmail.com>
2019-07-11 11:01:46 +0530

    password strength added

--- Sources/Validator/Rules/PasswordRule.swift
+++ Sources/Validator/Rules/PasswordRule.swift
@@ -13,25 +13,49 @@ import Foundation
  */
 public class PasswordRule : RegexRule {
     
-    // Alternative Regexes
-    
-    // 8 characters. One uppercase. One Lowercase. One number.
-    // static let regex = "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z]).{8,}$"
-    //
-    // no length. One uppercase. One lowercae. One number.
-    // static let regex = "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z]).*?$"
-    
-    /// Regular express string to be used in validation.
-    static let regex = "^(?=.*?[A-Z]).{8,}$"
+    /// Strength of password
+    public enum Strength {
+        /// 5 characters.
+        case weak
+        /// 8 characters. One uppercase.
+        case medium
+        /// 8 characters. One uppercase. One Lowercase. One number.
+        case strong
+        
+        /// Regular express string to be used in validation.
+        func regex() -> String {
+            switch self {
+            case .weak:
+                return "^.{5,}$"
+            case .medium:
+                return "^(?=.*?[A-Z]).{8,}$"
+            case .strong:
+                return "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z]).{8,}$"
+            }
+        }
+        
+        /// Error message for each strength types.
+        func message() -> String {
+            switch self {
+            case .weak:
+                return "is invalid. it must contain 5 characters"
+            case .medium:
+                return "is invalid. it must contain 8 characters with 1 uppercase"
+            case .strong:
+                return "is invalid. it must contain 8 characters with 1 uppercase, 1 lowercase and 1 number"
+            }
+        }
+    }
     
     /**
      Initializes a `PasswordRule` object that will validate a field is a valid password.
      
+     - parameter strength: `PasswordRule.Strength`.
      - parameter message: String of error message.
      - returns: An initialized `PasswordRule` object, or nil if an object could not be created for some reason that would not result in an exception.
      */
-    public init(message: String = "Must be 8 characters with 1 uppercase") {
-        super.init(regex: PasswordRule.regex, message: message)
+    public convenience init(strength: PasswordRule.Strength = .weak, message: String = "") {
+        self.init(regex: strength.regex(), message: message == "" ? strength.message() : message)
     }
     
 }
@@ -39,10 +63,8 @@ public class PasswordRule : RegexRule {
 public extension ValidationRule {
     
     /// Quick accessor for `PasswordRule`
-    public class var password: ValidationRule {
-        get  {
-            return PasswordRule()
-        }
+    public class func password(strength: PasswordRule.Strength = .weak) -> ValidationRule {
+        return PasswordRule(strength: strength)
     }
     
 }
