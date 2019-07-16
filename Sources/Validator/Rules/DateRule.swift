4cd39a5df11907d67db19d089d1c921c3e3fac7f
Soumen Bhuin <smbhuin@gmail.com>
2019-07-16 21:52:49 +0530

    date rules added

---
+++ Sources/Validator/Rules/DateRule.swift
@@ -0,0 +1,55 @@
+//
+//  DateRule.swift
+//  Validator
+//
+//  Created by Soumen Bhuin on 16/07/19.
+//  Copyright © 2019 SMB. All rights reserved.
+//
+
+import Foundation
+
+/**
+ `DateRule` is a subclass of `ValidationRule` that means value can't be nil or blank. Make sure required rule is the first item in rules list
+ */
+public class DateRule: ValidationRule<String> {
+    
+    /// Date format
+    private var format: String
+    
+    /**
+     Initializes a `DateRule` object that will validate a value is not nil.
+     
+     - parameter message: String of error message.
+     - returns: An initialized `DateRule` object, or nil if an object could not be created for some reason that would not result in an exception.
+     */
+    public init(format: String, message: String = "is invalid") {
+        self.format = format
+        super.init(message: message)
+    }
+    
+    /**
+     Method used to validate the provided value.
+     
+     - parameter value: Any value to be checked for validation.
+     - returns: `ValidationError`. nil if validation is successful; `ValidationError` if validation fails. Returns error for empty string or collection
+     */
+    public override func validate(_ value: String?) -> ValidationError? {
+        guard let v = value else { return nil }
+        let df = DateFormatter()
+        df.dateFormat = format
+        if let _ = df.date(from: v) {
+            return nil
+        }
+        return ValidationError(self.message)
+    }
+    
+}
+
+public extension ValidationRule {
+    
+    /// Quick accessor for `DateRule`
+    class func date(format: String = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") -> ValidationRule<String> {
+        return DateRule(format: format)
+    }
+    
+}
