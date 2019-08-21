b0ce3519f098744c2ae8025cff72842f3ea39bcc
Soumen Bhuin <smbhuin@gmail.com>
2019-08-21 15:20:43 +0530

    fixed

--- Sources/Rules/ArrayRule.swift
+++ Sources/Rules/ArrayRule.swift
@@ -11,10 +11,10 @@ import Foundation
 /**
  `ArrayRule` is a subclass of ValidationRule that defines how check if all array elements are valid.
  */
-public class ArrayRule: ValidationRule<[Any]> {
+public class ArrayRule<V>: ValidationRule<Array<V>> {
     
     /// Rules to be applied for each array elements
-    let rules: [ValidationRule<Any>]
+    let rules: [ValidationRule<V>]
     
     /**
      Initializes a `ArrayRule` object to validate that provided array has valid elements.
@@ -22,7 +22,7 @@ public class ArrayRule: ValidationRule<[Any]> {
      - parameter message: String of error message.
      - returns: An initialized object, or nil if an object could not be created for some reason that would not result in an exception.
      */
-    public init(message: String = "contains invalid element.", rules: [ValidationRule<Any>]){
+    public init(message: String = "contains invalid element.", rules: [ValidationRule<V>]){
         self.rules = rules
         super.init(message: message)
     }
@@ -33,7 +33,7 @@ public class ArrayRule: ValidationRule<[Any]> {
      - parameter value: Array to be checked for validation.
      - returns: Error Message. nil if validation is successful; `String` if validation fails.
      */
-    public override func validate(_ value: [Any]?) -> String? {
+    public override func validate(_ value: Array<V>?) -> String? {
         guard let v = value, !v.isEmpty else { return nil }
         let error = self.message
         for e in v {
@@ -51,7 +51,7 @@ public class ArrayRule: ValidationRule<[Any]> {
 public extension ValidationRule {
     
     /// Quick accessor for `ArrayRule`
-    class func array(rules: [ValidationRule<Any>]) -> ValidationRule<[Any]> {
+    class func array<V>(rules: [ValidationRule<V>]) -> ValidationRule<[V]> {
         return ArrayRule(rules: rules)
     }
     
